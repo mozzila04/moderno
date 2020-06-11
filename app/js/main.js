49d3d85c0b0fc3a51071b3fa78e7b72ed7ca3443
mozzila04 <sergiu.ciobanu92@gmail.com>
2020-06-11 21:56:07 +0100

    added new items

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,38 @@
-$(function(){
+$(function () {
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px"
+  });
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
+  $('.icon-th-list').on('click', function () {
+    $('.product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
 
-    
 
+  $('.icon-th-large').on('click', function () {
+    $('.product__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');    
+  });
+
+  var mixer = mixitup('.products__inner-box');
+
+
+});
+
+$(".js-range-slider").ionRangeSlider({
+  type: "double",
+  grid: false,
+  min: 0,
+  max: 1000,
+  from: 0,
+  to: 600,
+  prefix: "$"
 });
